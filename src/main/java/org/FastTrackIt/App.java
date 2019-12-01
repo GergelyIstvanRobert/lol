74501c737ccbdd7fd1db46d1e746f32cc8ad6463
GergelyIstvanRobert <skiridershop@gmail.com>
2019-12-01 10:33:48 +0200

    Initial commit

---
+++ src/main/java/org/FastTrackIt/App.java
@@ -0,0 +1,25 @@
+package org.FastTrackIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int x = 13;
+        int y = 7;
+        int result;
+        result = x + y;
+        System.out.println("The result is " + result);
+        result = x - y;
+        System.out.println("The result is " + result);
+        result = x / y;
+        System.out.println("The result is " + result);
+        result = x*y;
+        System.out.println( "The result is " + result);
+
+    }
+}
---
+++ src/main/java/org/FastTrackIt/Calcule.java
@@ -0,0 +1,8 @@
+package org.FastTrackIt;
+
+public class Calcule {
+    int x = 13;
+    int y = 7;
+
+
+}
